e9b57aa23435d0141763d58c05e006db29dd6268
Ravindra <sirviravindra609@gmail.com>
2024-05-08 08:45:25 +0530

    chore: Remove unused portfolio page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3af70b8598660346"; }
+/******/ 		__webpack_require__.h = function() { return "09d3abb6da267857"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1b45768ce8c9633c"; }
+/******/ 	__webpack_require__.h = function() { return "09d3abb6da267857"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
